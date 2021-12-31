5c9023d70bf124bd52cf30f7b19eee0933c567ce
0987714046 <cuongnguyen10071211@gmail.com>
2021-12-30 20:20:54 -0500

    nop bai

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,13 +1,13 @@
     $.ajax({
         type: "GET",
         url: "https://61cc05a6198df60017aebe26.mockapi.io/api/Netflix",
-        success: function(dataMoviesSmall) {
-            renderMoviesSmall(dataMoviesSmall)
+        success: function(dataMovies) {
+            rs(dataMovies)
         },
     });
 
 
-    function renderMoviesSmall(Netflix) {
+    function rs(Netflix) {
         var html = Netflix.map(Netflix => {
             return `<img src="${Netflix.image}" class="row_poster">
         
